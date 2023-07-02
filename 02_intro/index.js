71514aff97da1ed7ca2aa7ea837a1c935530e923
sergeyFrontend <utemishevsergey02@gmail.com>
2023-07-02 20:31:00 +0500

    homework module third

--- 02_intro/index.js
+++ 02_intro/index.js
@@ -1,6 +1,4 @@
-
 //INFORMATION FROM LESSON
-
 
 // // let a = 25;
 // // let b = 25.01;
@@ -35,12 +33,6 @@
 // Math.max(10, 11, 12, 1); //12
 // Math.min(1, 2, 3, -6); //-6
 
-
-
-
-
-
-
 // HOME WORK
 
 //ex.1
@@ -48,15 +40,14 @@
 let x1 = 5;
 let x2 = 8;
 
-let y1= 11;
+let y1 = 11;
 let y2 = 9;
 
-const width = Math.abs(x2-x1);
-const height = Math.abs(y2-y1);
+const width = Math.abs(x2 - x1);
+const height = Math.abs(y2 - y1);
 const area = width * height;
 
-console.log('площадь прямоугольника равна:',area)
-
+// console.log('площадь прямоугольника равна:',area)
 
 //ex.2
 
@@ -64,45 +55,38 @@ let a = 13.123456789;
 let b = 13.123456789;
 let precision = 2;
 
-
 let firstNum = a - Math.floor(a);
 let secondNum = b - Math.floor(b);
 
-let firstNumNormalize = Math.round(firstNum * Math.pow(10,precision));
-let secondNumNormalize = Math.round(secondNum * Math.pow(10,precision));
+let firstNumNormalize = Math.round(firstNum * Math.pow(10, precision));
+let secondNumNormalize = Math.round(secondNum * Math.pow(10, precision));
 
+// console.log('Первое число', firstNumNormalize);
+// console.log('Второе число', secondNumNormalize);
 
-
-console.log('Первое число', firstNumNormalize);
-console.log('Второе число', secondNumNormalize);
-
-console.log('Исходные числа равны:',firstNumNormalize === secondNumNormalize);
-console.log('Первое число меньше второго', firstNumNormalize < secondNumNormalize);
-console.log('Первое число больше второго',firstNumNormalize > secondNumNormalize);
-console.log('Первое число меньше или равно второму',firstNumNormalize <= secondNumNormalize);
-console.log('Первое число больше или равно  второго',firstNumNormalize >= secondNumNormalize);
-console.log('Числа не равны ',firstNumNormalize !== secondNumNormalize);
-
+// console.log('Исходные числа равны:',firstNumNormalize === secondNumNormalize);
+// console.log('Первое число меньше второго', firstNumNormalize < secondNumNormalize);
+// console.log('Первое число больше второго',firstNumNormalize > secondNumNormalize);
+// console.log('Первое число меньше или равно второму',firstNumNormalize <= secondNumNormalize);
+// console.log('Первое число больше или равно  второго',firstNumNormalize >= secondNumNormalize);
+// console.log('Числа не равны ',firstNumNormalize !== secondNumNormalize);
 
 // ex.3
 
 const m = 12.65;
 const n = 11.11;
 
-
 let range = Math.abs(m - n);
 
-const randomNumberOne = Math.floor(Math.random() * range + Math.min(m,n));
-const randomNumberTwo = Math.floor(Math.random() * range + Math.min(m,n));
+const randomNumberOne = Math.floor(Math.random() * range + Math.min(m, n));
+const randomNumberTwo = Math.floor(Math.random() * range + Math.min(m, n));
 
-console.log('Первое число', randomNumberOne);
-console.log('Второе число', randomNumberTwo);
+// console.log('Первое число', randomNumberOne);
+// console.log('Второе число', randomNumberTwo);
 
-console.log('Исходные числа равны:',randomNumberOne === randomNumberTwo);
-console.log('Первое число меньше второго', randomNumberOne < randomNumberTwo);
-console.log('Первое число больше второго',randomNumberOne > randomNumberTwo);
-console.log('Первое число меньше или равно второму',randomNumberOne <= randomNumberTwo);
-console.log('Первое число больше или равно  второго',randomNumberOne >= randomNumberTwo);
-console.log('Числа не равны ',randomNumberOne !== randomNumberTwo);
-
-
+// console.log('Исходные числа равны:',randomNumberOne === randomNumberTwo);
+// console.log('Первое число меньше второго', randomNumberOne < randomNumberTwo);
+// console.log('Первое число больше второго',randomNumberOne > randomNumberTwo);
+// console.log('Первое число меньше или равно второму',randomNumberOne <= randomNumberTwo);
+// console.log('Первое число больше или равно  второго',randomNumberOne >= randomNumberTwo);
+// console.log('Числа не равны ',randomNumberOne !== randomNumberTwo);
